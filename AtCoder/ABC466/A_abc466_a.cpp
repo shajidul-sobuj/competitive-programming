028da51613d33c50acfe515ccb3faa9245efd70b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:41:12 +0600

    [AC] AtCoder ABC466A - abc466 a

---
+++ AtCoder/ABC466/A_abc466_a.cpp
@@ -0,0 +1,46 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC466
+ * Problem: A - abc466 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:41:11
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+const int N = 1e5+10;
+
+void boom()
+{
+    int n; cin >> n;
+    while(n--){
+        int x; cin >> x;
+        if (x>=0) {
+            cout << "No\n";
+            return;
+        }
+        
+    }
+    cout << "Yes\n";
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        boom();
+    }
+
+    return 0;
+}
