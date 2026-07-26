b2ef6c27472d396f0a996193b2352a3d597eee42
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:22:01 +0600

    [AC 800] Codeforces 2167A - Square?

---
+++ Codeforces/Codeforces_Round_1062_(Div._4)/A_Square.cpp
@@ -0,0 +1,45 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ *
+ * Problem: 2167A - Square?
+ * Link: https://codeforces.com/contest/2167/problem/A
+ * Submission ID: 382141569 (https://codeforces.com/contest/2167/submission/382141569)
+ * Language: C++23 (GCC 14-64, msys2)
+ * Execution Time: 46 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: math, sortings
+ * Submitted At: 2026-07-11 17:28:34 UTC
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+const int N = 1e5+10;
+
+void boom()
+{
+    int a,b,c,d; cin >> a >> b >> c >> d;
+    if (a == b && b == c && c == d) cout << "YES\n";
+    else cout << "NO\n";
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        boom();
+    }
+
+    return 0;
+}
