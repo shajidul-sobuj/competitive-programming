ca3eb60db881437f95dfc5c9d21db11ca662d289
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:36:52 +0600

    [AC] AtCoder ABC445B - abc445 b

---
+++ AtCoder/ABC445/B_abc445_b.cpp
@@ -0,0 +1,33 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC445
+ * Problem: B - abc445 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:36:51
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <string> s(n);
+    for (int i=0;i<n;i++) cin >> s[i];
+    int mx_length = INT_MIN;
+    for (int i=0;i<n;i++)
+    {
+        mx_length = max(mx_length,(int)s[i].size());
+    }
+    for (int i=0;i<n;i++)
+    {
+        int x = mx_length - s[i].size();
+        int left = x/2;
+        int right = x-left;
+        cout << string(left,'.');
+        cout << s[i];
+        cout << string(right,'.');
+        cout << endl;
+    }
+    return 0;
+}
