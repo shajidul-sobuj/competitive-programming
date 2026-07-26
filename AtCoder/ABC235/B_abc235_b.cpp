41431b69204fcbb64f41d0eff78594ada45adce9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:35:39 +0600

    [AC] AtCoder ABC235B - abc235 b

---
+++ AtCoder/ABC235/B_abc235_b.cpp
@@ -0,0 +1,33 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC235
+ * Problem: B - abc235 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:35:38
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i=0;i<n;i++)
+    {
+        cin >>a[i];
+    }
+    int tl = a[0];
+    for (int i=0;i<n-1;i++)
+    {
+        if (a[i]<a[i+1])
+        {
+            tl = a[i+1];
+        }
+        else{
+            break;
+        }
+    }
+    cout << tl << endl;
+    return 0;
+}
