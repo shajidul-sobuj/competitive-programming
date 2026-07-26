90872a49613f5e95d9a5a4d442c0509837e28cff
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:21:48 +0600

    [AC Unrated] Codeforces 2246A - farmpiggie and Subset Sum

---
+++ Codeforces/Codeforces_Round_1108_(Div._2)/A_farmpiggie_and_Subset_Sum.cpp
@@ -0,0 +1,45 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ *
+ * Problem: 2246A - farmpiggie and Subset Sum
+ * Link: https://codeforces.com/contest/2246/problem/A
+ * Submission ID: 382244384 (https://codeforces.com/contest/2246/submission/382244384)
+ * Language: C++23 (GCC 14-64, msys2)
+ * Execution Time: 15 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: Unrated | Tags: constructive algorithms
+ * Submitted At: 2026-07-12 14:47:50 UTC
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+const int N = 1e5+10;
+
+void boom()
+{
+    int n; cin >> n;
+    for (int i=2;i<=n;i++) cout << i << " ";
+    cout << 1 << endl;
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        boom();
+    }
+
+    return 0;
+}
