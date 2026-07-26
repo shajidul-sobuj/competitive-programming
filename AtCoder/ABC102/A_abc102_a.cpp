579c53f4eaa162684d57f7aa3d59469806225022
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:38:02 +0600

    [AC] AtCoder ABC102A - abc102 a

---
+++ AtCoder/ABC102/A_abc102_a.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC102
+ * Problem: A - abc102 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:38:02
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n;
+    cin >> n;
+    if (n%2==0)
+    {
+        cout << n << endl;
+    }else{
+        cout << (n*2) << endl;
+    }
+    return 0;
+}
