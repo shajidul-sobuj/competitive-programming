447c31aae11d6354b3e458ad5fe5b463c8b9545f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:34:50 +0600

    [AC] AtCoder ABC235A - abc235 a

---
+++ AtCoder/ABC235/A_abc235_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC235
+ * Problem: A - abc235 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:34:49
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << 111*(s[0]-'0'+s[1]-'0'+s[2]-'0') << endl;
+    return 0;
+}
