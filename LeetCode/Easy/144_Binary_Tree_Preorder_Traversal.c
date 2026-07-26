4ccb328fc7de50abde66be674a7f057896ea131b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 04:06:46 +0600

    [AC] LeetCode Easy144 - Binary Tree Preorder Traversal

---
+++ LeetCode/Easy/144_Binary_Tree_Preorder_Traversal.c
@@ -0,0 +1,18 @@
+/**
+ * Platform: LeetCode
+ * Problem: 144. Binary Tree Preorder Traversal
+ * Difficulty: Easy
+ * Language: cpp
+ * Date: 2026-07-26 22:06:46
+ */
+
+/**
+ * Platform: LeetCode
+ * Problem: 144. Binary Tree Preorder Traversal
+ * Difficulty: Easy
+ * Language: cpp
+ * Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Binary Tree Preorder Traversal (144)
