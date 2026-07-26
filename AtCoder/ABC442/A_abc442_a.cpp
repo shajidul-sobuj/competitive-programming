b9da356065b1d9f6107502f4c5aed09f83bcba4f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:35:47 +0600

    [AC] AtCoder ABC442A - abc442 a

---
+++ AtCoder/ABC442/A_abc442_a.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC442
+ * Problem: A - abc442 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:35:46
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int count = 0;
+    for (int i=0;i<s.size();i++)
+    {
+        if(s[i]=='i'|| s[i]=='j') count++;
+    }
+    cout << count << endl;
+    return 0;
+}
