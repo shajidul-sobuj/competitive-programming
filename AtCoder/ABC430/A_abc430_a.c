8c099c6d1c5788a11d5c2d11a06d64754d81a4ba
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:33:54 +0600

    [AC] AtCoder ABC430A - abc430 a

---
+++ AtCoder/ABC430/A_abc430_a.c
@@ -0,0 +1,23 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC430
+ * Problem: A - abc430 a
+ * Language: C23 (Clang 21.1.0)
+ * Date: 2026-07-26 21:33:53
+ */
+
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if (c>=a && d<b){
+        printf("Yes\n");
+    }else {
+        printf("No\n");
+    }
+    return 0;
+}
+
+
+
