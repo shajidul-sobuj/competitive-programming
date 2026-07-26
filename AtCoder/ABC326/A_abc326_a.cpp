3521499c60f1fc4a3009c84fb2754c71e86a712f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:34:25 +0600

    [AC] AtCoder ABC326A - abc326 a

---
+++ AtCoder/ABC326/A_abc326_a.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC326
+ * Problem: A - abc326 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:34:24
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    int t = x-y;
+    if ((t > 0 && t <= 3) || ((t < 0 && t >= -2))) {
+        cout << "Yes\n";
+    }else {
+        cout << "No\n";
+    }
+    return 0;
+}
