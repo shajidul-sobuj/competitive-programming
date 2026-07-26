b2161c61a5fc2cd503b9539e7b278197badf24e1
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:37:53 +0600

    [AC] AtCoder ABC214B - abc214 b

---
+++ AtCoder/ABC214/B_abc214_b.cpp
@@ -0,0 +1,29 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC214
+ * Problem: B - abc214 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:37:52
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int s,t;
+    cin >> s >> t;
+    int count = 0;
+    for (int i=0;i<=s;i++)
+    {
+        for (int j=0;j<=s;j++)
+        {
+            for (int k=0;k<=s;k++)
+            {
+                if ( (i+j+k <= s) &&  (i*j*k <= t))
+                    count++;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
