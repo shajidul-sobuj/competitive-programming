512194a9625551d47ff2fb7f515d40a6de56992d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:35:12 +0600

    [AC] AtCoder ABC440B - abc440 b

---
+++ AtCoder/ABC440/B_abc440_b.cpp
@@ -0,0 +1,42 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC440
+ * Problem: B - abc440 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:35:11
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> a(n+1);
+    for (int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    int t=3;
+    vector <int> v;
+    v = a;
+    sort(v.begin(),v.end());
+    for (int i=1;i<=n;i++){
+        if (a[i]==v[1]) {
+            cout << i << " ";
+            break;
+        }
+    }
+    for (int i=1;i<=n;i++){
+        if (a[i]==v[2]) {
+            cout << i << " ";
+            break;
+        }
+    }
+    for (int i=1;i<=n;i++){
+        if (a[i]==v[3]) {
+            cout << i << " ";
+            break;
+        }
+    }
+    return 0;
+}
