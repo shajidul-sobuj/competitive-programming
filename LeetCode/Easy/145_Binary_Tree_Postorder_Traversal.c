48afa630bfb3f2de025ade4950c514af2ee958d1
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 04:06:55 +0600

    [AC] LeetCode Easy145 - Binary Tree Postorder Traversal

---
+++ LeetCode/Easy/145_Binary_Tree_Postorder_Traversal.c
@@ -0,0 +1,18 @@
+/**
+ * Platform: LeetCode
+ * Problem: 145. Binary Tree Postorder Traversal
+ * Difficulty: Easy
+ * Language: cpp
+ * Date: 2026-07-26 22:06:54
+ */
+
+/**
+ * Platform: LeetCode
+ * Problem: 145. Binary Tree Postorder Traversal
+ * Difficulty: Easy
+ * Language: cpp
+ * Link: https://leetcode.com/problems/binary-tree-postorder-traversal/
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Binary Tree Postorder Traversal (145)
