f38a99c4fc6d85f8c2b15ae359b03ec562115b66
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:35:21 +0600

    [AC] AtCoder ABC289A - abc289 a

---
+++ AtCoder/ABC289/A_abc289_a.cpp
@@ -0,0 +1,21 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC289
+ * Problem: A - abc289 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:35:20
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    for (char c : s)
+    {
+        if (c=='0') cout << "1";
+        else cout << "0";
+    }
+    return 0;
+}
