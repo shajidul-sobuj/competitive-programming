1acf1569bef2f57b505301297539f8ce1b633010
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:36:35 +0600

    [AC] AtCoder AWC0003A - awc0003 a

---
+++ AtCoder/AWC0003/A_awc0003_a.cpp
@@ -0,0 +1,24 @@
+/**
+ * Platform: AtCoder
+ * Contest: AWC0003
+ * Problem: A - awc0003 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:36:34
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n,k;
+    cin >> n >> k;
+    long long int count = 0;
+    while(n--)
+    {
+        long long int x,y;
+        cin >> x >> y;
+        if (x*y>=k) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
