35ffbf828d84e2a34353384c645356c1840e2618
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:36:10 +0600

    [AC] AtCoder AWC0001A - awc0001 a

---
+++ AtCoder/AWC0001/A_awc0001_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Contest: AWC0001
+ * Problem: A - awc0001 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:36:09
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n;
+    cin >> n;
+    cout << (n+1) << endl;
+    return 0;
+}
