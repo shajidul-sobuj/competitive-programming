d26a68cf2ffc1f1dcd1ba2bf8349939a00c51ab7
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:36:18 +0600

    [AC] AtCoder AWC0001B - awc0001 b

---
+++ AtCoder/AWC0001/B_awc0001_b.cpp
@@ -0,0 +1,29 @@
+/**
+ * Platform: AtCoder
+ * Contest: AWC0001
+ * Problem: B - awc0001 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:36:17
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n,l,r;
+    cin >> n >> l >> r;
+    vector <long long int> v(n+1);
+    for (int i=1;i<=n;i++) cin >> v[i];
+    int pos = -1;
+    long long int mx = LLONG_MIN;
+    for (int i=1;i<=n;i++)
+    {
+        if (v[i]>=l && v[i]<=r && v[i]>mx)
+        {
+            mx = v[i];
+            pos = i;
+        }
+    }
+    cout << pos << endl;
+    return 0;
+}
