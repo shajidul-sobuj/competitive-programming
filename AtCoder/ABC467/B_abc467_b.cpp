10f51d86bbb51a79cd79a54a1c01acdfb34aaa54
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:41:34 +0600

    [AC] AtCoder ABC467B - abc467 b

---
+++ AtCoder/ABC467/B_abc467_b.cpp
@@ -0,0 +1,31 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC467
+ * Problem: B - abc467 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:41:33
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+const int N = 1e5+10;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    int ans = 0;
+    while(n--){
+        int x,y; cin >> x >> y;
+        string s; cin >> s;
+        if(s == "keep") ans += (y-x);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
