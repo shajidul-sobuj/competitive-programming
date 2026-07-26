5603aef5f13c21d9252dcf30c9c333ff2a34faad
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:35:55 +0600

    [AC] AtCoder ABC444A - abc444 a

---
+++ AtCoder/ABC444/A_abc444_a.cpp
@@ -0,0 +1,23 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC444
+ * Problem: A - abc444 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:35:54
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[0]==s[1] &&  s[0]==s[2])
+    {
+        cout << "Yes\n";
+    }else{
+        cout << "No\n";
+    }
+    
+    return 0;
+}
