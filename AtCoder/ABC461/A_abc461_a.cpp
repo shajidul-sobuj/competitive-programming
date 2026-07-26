a0e0eda23f361522f0aa8979bc6499ff8929b6d4
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:40:45 +0600

    [AC] AtCoder ABC461A - abc461 a

---
+++ AtCoder/ABC461/A_abc461_a.cpp
@@ -0,0 +1,25 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC461
+ * Problem: A - abc461 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:40:44
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b; cin >> a >> b;
+    if (a<=b) cout << "Yes\n";
+    else cout <<"No\n";
+
+    return 0;
+}
