60907cda748a1144004d6d4d9b5caf1b4f60f187
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 04:06:33 +0600

    [AC] LeetCode Easy1 - Two Sum

---
+++ LeetCode/Easy/1_Two_Sum.c
@@ -0,0 +1,18 @@
+/**
+ * Platform: LeetCode
+ * Problem: 1. Two Sum
+ * Difficulty: Easy
+ * Language: cpp
+ * Date: 2026-07-26 22:06:32
+ */
+
+/**
+ * Platform: LeetCode
+ * Problem: 1. Two Sum
+ * Difficulty: Easy
+ * Language: cpp
+ * Link: https://leetcode.com/problems/two-sum/
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Two Sum (1)
