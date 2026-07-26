db136019b50b8ce336f8f5163e5e699a15c7cec8
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:18:43 +0600

    [AC 900] Codeforces 1201A - Important Exam

---
+++ Codeforces/Codeforces_Round_577_(Div._2)/A_Important_Exam.cpp
@@ -0,0 +1,47 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ *
+ * Problem: 1201A - Important Exam
+ * Link: https://codeforces.com/contest/1201/problem/A
+ * Submission ID: 383632006 (https://codeforces.com/contest/1201/submission/383632006)
+ * Language: C++23 (GCC 14-64, msys2)
+ * Execution Time: 62 ms | Memory: 100 KB
+ * Verdict: OK
+ * Rating: 900 | Tags: implementation, strings
+ * Submitted At: 2026-07-20 21:41:19 UTC
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+const int N = 1e5+10;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    int m; cin >> m;
+    vector<string> s(n);
+    vector<int> a(m);
+    for (auto &ss:s) cin >> ss;
+    for (auto &x:a) cin >> x;
+    ll ans = 0;
+    for(int i=0;i<m;i++){
+        map<char, int> mp;
+        for (int j=0;j<n;j++){
+            mp[s[j][i]]++;
+        }
+        int mx = 0;
+        for (auto xp:mp){
+            mx = max(mx, xp.second);
+        }
+        ans+=(mx*a[i]);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
