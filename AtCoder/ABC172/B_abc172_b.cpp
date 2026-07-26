a7b55afd2d3c79094ea49b6fe951feec94856e88
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:37:28 +0600

    [AC] AtCoder ABC172B - abc172 b

---
+++ AtCoder/ABC172/B_abc172_b.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC172
+ * Problem: B - abc172 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:37:27
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s,t;
+    cin >> s >> t;
+    int count = 0;
+    for (int i=0;i<s.size();i++)
+    {
+        if (s[i]!=t[i]) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
