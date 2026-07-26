9c6d68f7b440df688ccb5f6ee61425ca7710fd00
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:37:36 +0600

    [AC] AtCoder ABC220A - abc220 a

---
+++ AtCoder/ABC220/A_abc220_a.cpp
@@ -0,0 +1,25 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC220
+ * Problem: A - abc220 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:37:35
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int a,b,k;
+    cin >> a >> b >> k;
+    for (long long int i=a;i<=b;i++)
+    {
+        if (i%k == 0)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+    return 0;
+}
