b03462d463f8e94e4b48e87c3e8e5aad05693a29
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:37:44 +0600

    [AC] AtCoder ABC125A - abc125 a

---
+++ AtCoder/ABC125/A_abc125_a.cpp
@@ -0,0 +1,19 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC125
+ * Problem: A - abc125 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:37:44
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,k;
+    cin >> a >> b >> k;
+    k = k+0.5;
+    int ans = (k/a)*b;
+    cout << (int)ans << endl;
+    return 0;
+}
