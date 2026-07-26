1c50d2c6b206deb3686d8d99b866773c1f209796
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:37:11 +0600

    [AC] AtCoder ABC124A - abc124 a

---
+++ AtCoder/ABC124/A_abc124_a.cpp
@@ -0,0 +1,25 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC124
+ * Problem: A - abc124 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:37:10
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int x,y;
+    cin >> x >> y;
+
+    if (x<y)
+        swap(x,y);
+    
+    long long int ans = x;
+    x--;
+    ans+=max(x,y);
+    cout << ans << endl;
+
+    return 0;
+}
