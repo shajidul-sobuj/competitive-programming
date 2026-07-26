cd861152d297e34291e100217bf2593f1fcdcdb1
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:40:14 +0600

    [AC] AtCoder ABC454A - abc454 a

---
+++ AtCoder/ABC454/A_abc454_a.cpp
@@ -0,0 +1,25 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC454
+ * Problem: A - abc454 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:40:12
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int l,r;
+    cin >> l >> r;
+    cout << (r-l+1) << endl;
+
+    return 0;
+}
