f07a592fa5a9c1a5cea79709bc13e98e73bde1b7
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:38:27 +0600

    [AC] AtCoder ABC064A - abc064 a

---
+++ AtCoder/ABC064/A_abc064_a.cpp
@@ -0,0 +1,20 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC064
+ * Problem: A - abc064 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:38:26
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    long long x = c+(b*10)+(a*100);
+    if (x%4==0) cout << "YES\n";
+    else cout << "NO\n";
+    // cout << x << endl;
+    return 0;
+}
