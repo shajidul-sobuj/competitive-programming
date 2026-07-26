1dbde373d5b1ea5da1d07901537796c9c675a707
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:40:56 +0600

    [AC] AtCoder ABC461B - abc461 b

---
+++ AtCoder/ABC461/B_abc461_b.cpp
@@ -0,0 +1,42 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC461
+ * Problem: B - abc461 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:40:55
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<int> a(n);
+    vector<pair<int, int>> b(n);
+    for (int i=0;i<n;i++) cin >> a[i];
+    for (int i=0;i<n;i++)
+    {   
+        int x; cin >> x;
+        b[i].first = x;
+        b[i].second = i;
+    }
+    sort(b.begin(),b.end());
+    for (int i=0;i<n;i++)
+    {
+        if (a[i]!=b[i].second+1)
+        {
+            cout << "No\n";
+            return 0;
+        }
+    }
+    cout << "Yes\n";
+
+    return 0;
+}
