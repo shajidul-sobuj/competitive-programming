6a47bd85a7c8012acba9b248dbc451b86d717c53
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 04:06:27 +0600

    [AC] LeetCode Easy9 - Palindrome Number

---
+++ LeetCode/Easy/9_Palindrome_Number.c
@@ -0,0 +1,18 @@
+/**
+ * Platform: LeetCode
+ * Problem: 9. Palindrome Number
+ * Difficulty: Easy
+ * Language: cpp
+ * Date: 2026-07-26 22:06:25
+ */
+
+/**
+ * Platform: LeetCode
+ * Problem: 9. Palindrome Number
+ * Difficulty: Easy
+ * Language: cpp
+ * Link: https://leetcode.com/problems/palindrome-number/
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Palindrome Number (9)
