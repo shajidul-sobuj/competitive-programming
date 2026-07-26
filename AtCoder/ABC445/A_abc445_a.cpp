37138599c9ecb02a372a2f47b1b7573acddd3813
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:36:44 +0600

    [AC] AtCoder ABC445A - abc445 a

---
+++ AtCoder/ABC445/A_abc445_a.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC445
+ * Problem: A - abc445 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:36:43
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[0]==s[s.size()-1])
+    {
+        cout << "Yes\n";
+    }else {
+        cout << "No\n";
+    }
+    return 0;
+}
