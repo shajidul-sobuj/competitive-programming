168507670582ef24ce929d07a58c83aeabf9d138
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:35:05 +0600

    [AC] AtCoder ABC440A - abc440 a

---
+++ AtCoder/ABC440/A_abc440_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC440
+ * Problem: A - abc440 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:35:04
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    cout << (long long int)(x*(pow(2,y))) << endl;
+    return 0;
+}
