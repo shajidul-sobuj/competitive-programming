04dcff11321c706a38f37c97976b9c9338e8b7b9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:39:17 +0600

    [AC] AtCoder ABC450A - abc450 a

---
+++ AtCoder/ABC450/A_abc450_a.cpp
@@ -0,0 +1,27 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC450
+ * Problem: A - abc450 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:39:16
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i=n;i>1;i--)
+    {
+        cout << i << ",";
+    }
+    cout << 1 << endl;
+    return 0;
+}
