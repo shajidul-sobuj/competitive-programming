73e5a065e7272d6818bd7dbd4786e106d3069a0e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:41:43 +0600

    [AC] AtCoder ABC467A - abc467 a

---
+++ AtCoder/ABC467/A_abc467_a.cpp
@@ -0,0 +1,29 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC467
+ * Problem: A - abc467 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:41:42
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+void boom()
+{
+    int h,w; cin >> h >> w;
+    if((w*10000)>=(25*h*h)) cout << "Yes\n";
+    else cout << "No\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while(t--) boom();
+    return 0;
+}
