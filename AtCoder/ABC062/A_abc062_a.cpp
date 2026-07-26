60bf1d863831feda2897850760b4c36855935e50
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:38:19 +0600

    [AC] AtCoder ABC062A - abc062 a

---
+++ AtCoder/ABC062/A_abc062_a.cpp
@@ -0,0 +1,37 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC062
+ * Problem: A - abc062 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:38:18
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    
+    vector<int> a = {1,3,5,7,10,12};
+    vector<int> b = {4,6,9,11};
+
+    vector<int> ck(13);
+    for (int t : a)
+    {
+        ck[t]=0;
+    }
+    for (int t : b)
+    {
+        ck[t]=1;
+    }
+    ck[2]=-1;
+
+    if (ck[x] == ck[y])
+    {
+        cout << "Yes\n";
+    }
+    else cout << "No\n";
+
+    return 0;
+}
