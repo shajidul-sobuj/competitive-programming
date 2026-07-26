4e13d39dc916466e835d00cad2b13576f1ef5c58
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:34:33 +0600

    [AC] AtCoder ABC149B - abc149 b

---
+++ AtCoder/ABC149/B_abc149_b.cpp
@@ -0,0 +1,26 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC149
+ * Problem: B - abc149 b
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:34:32
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b, k;
+    cin >> a >> b >> k;
+
+    if (k <= a) {
+        a -= k;
+    } else {
+        k -= a;
+        a = 0;
+        b = max(0LL, b - k);
+    }
+
+    cout << a << " " << b << endl;
+    return 0;
+}
