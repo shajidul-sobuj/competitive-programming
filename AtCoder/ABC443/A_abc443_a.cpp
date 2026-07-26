e304af9ce3761d2c766e03891916265bb0f7dcfe
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:36:03 +0600

    [AC] AtCoder ABC443A - abc443 a

---
+++ AtCoder/ABC443/A_abc443_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC443
+ * Problem: A - abc443 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:36:03
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s << "s" << endl;
+    return 0;
+}
