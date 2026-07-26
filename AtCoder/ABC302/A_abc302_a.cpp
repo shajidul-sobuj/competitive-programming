ac40efece70ed225f0e0e8abc4d3c68300c2c179
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:34:16 +0600

    [AC] AtCoder ABC302A - abc302 a

---
+++ AtCoder/ABC302/A_abc302_a.cpp
@@ -0,0 +1,21 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC302
+ * Problem: A - abc302 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:34:15
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a,b;
+    cin >> a >> b;
+    if (a%b==0) {
+        cout << a/b << endl;
+    }else {
+        cout << (a/b)+1 << endl;
+    }
+    return 0;
+}
