122b4764b8a92a6128962e6d697ddd680797fa4a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 03:34:41 +0600

    [AC] AtCoder ABC336A - abc336 a

---
+++ AtCoder/ABC336/A_abc336_a.cpp
@@ -0,0 +1,21 @@
+/**
+ * Platform: AtCoder
+ * Contest: ABC336
+ * Problem: A - abc336 a
+ * Language: C++23 (GCC 15.2.0)
+ * Date: 2026-07-26 21:34:41
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "L";
+    while(n--){
+        cout << "o";
+    }
+    cout << "ng\n";
+    return 0;
+}
