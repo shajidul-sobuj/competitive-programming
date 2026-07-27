60ffaca74a5a292148d61ca914c1bcd221b41f5a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 13:46:45 +0600

    [AC] VJudge CodeForces1444A - CodeForces_1444A

---
+++ VJudge/CodeForces/1444A_CodeForces_1444A.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_1444A
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 1444A
+ * Run ID: 71145397
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 1444A
