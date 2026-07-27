d7e2e29a082e32e55625ba12788bf3fe343c6908
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 13:46:38 +0600

    [AC] VJudge CodeChefNYRES - CodeChef_NYRES

---
+++ VJudge/CodeChef/NYRES_CodeChef_NYRES.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_NYRES
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: NYRES
+ * Run ID: 71330472
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - NYRES
