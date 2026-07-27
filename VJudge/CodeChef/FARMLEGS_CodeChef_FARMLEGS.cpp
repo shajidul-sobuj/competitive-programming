6354191ecadebc9fac9e15ef160b362e30240caa
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 13:46:12 +0600

    [AC] VJudge CodeChefFARMLEGS - CodeChef_FARMLEGS

---
+++ VJudge/CodeChef/FARMLEGS_CodeChef_FARMLEGS.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_FARMLEGS
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: FARMLEGS
+ * Run ID: 71330710
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - FARMLEGS
