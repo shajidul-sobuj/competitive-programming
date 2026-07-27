2a71307ed97b45b4dfffaab07a68517b98b3dfbb
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 13:45:39 +0600

    [AC] VJudge CodeChefEZSPEAK - CodeChef_EZSPEAK

---
+++ VJudge/CodeChef/EZSPEAK_CodeChef_EZSPEAK.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_EZSPEAK
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: EZSPEAK
+ * Run ID: 71331197
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - EZSPEAK
