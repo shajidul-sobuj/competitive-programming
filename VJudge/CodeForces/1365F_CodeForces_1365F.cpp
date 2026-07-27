2f5c11991a562d73fa660996a5723e4426baa0b9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 13:46:59 +0600

    [AC] VJudge CodeForces1365F - CodeForces_1365F

---
+++ VJudge/CodeForces/1365F_CodeForces_1365F.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_1365F
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 1365F
+ * Run ID: 71144660
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 1365F
