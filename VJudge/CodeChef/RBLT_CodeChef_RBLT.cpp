faca28d374d1db01962941557d43e082fbe6d1c3
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-27 13:45:50 +0600

    [AC] VJudge CodeChefRBLT - CodeChef_RBLT

---
+++ VJudge/CodeChef/RBLT_CodeChef_RBLT.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_RBLT
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: RBLT
+ * Run ID: 71331016
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - RBLT
