ac6431ddb394e5cd9e48e92e90e38b10bc7dfe87
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:49:30 +0600

    [AC] VJudge CodeForces1370B - CodeForces_1370B

---
+++ VJudge/CodeForces/1370B_CodeForces_1370B.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_1370B
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 1370B
+ * Run ID: 71415120
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 1370B
