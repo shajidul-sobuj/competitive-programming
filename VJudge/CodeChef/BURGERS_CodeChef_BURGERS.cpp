c03c3a79da0504dc111138226df9749f02d99ccd
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:53:53 +0600

    [AC] VJudge CodeChefBURGERS - CodeChef_BURGERS

---
+++ VJudge/CodeChef/BURGERS_CodeChef_BURGERS.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_BURGERS
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: BURGERS
+ * Run ID: 70810649
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - BURGERS
