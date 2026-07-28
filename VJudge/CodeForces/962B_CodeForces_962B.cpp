457dc74596138f409bace6746d91196a6ac5632c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:53:23 +0600

    [AC] VJudge CodeForces962B - CodeForces_962B

---
+++ VJudge/CodeForces/962B_CodeForces_962B.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_962B
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 962B
+ * Run ID: 70858337
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 962B
