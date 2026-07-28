398ab15f628842964125cf7bccfe84b53b459ef2
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:57:45 +0600

    [AC] VJudge CodeChefACCESS - CodeChef_ACCESS

---
+++ VJudge/CodeChef/ACCESS_CodeChef_ACCESS.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_ACCESS
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: ACCESS
+ * Run ID: 70568271
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - ACCESS
