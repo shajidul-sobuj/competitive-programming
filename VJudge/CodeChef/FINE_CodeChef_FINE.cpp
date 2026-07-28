36311eba785178520c76f1706a246378d5cd0406
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:51:38 +0600

    [AC] VJudge CodeChefFINE - CodeChef_FINE

---
+++ VJudge/CodeChef/FINE_CodeChef_FINE.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_FINE
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: FINE
+ * Run ID: 71089155
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - FINE
