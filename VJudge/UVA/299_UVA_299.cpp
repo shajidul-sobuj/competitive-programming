11f1ead97852780e0ca594377cc670f933eeb160
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:52 +0600

    [AC] VJudge UVA299 - UVA_299

---
+++ VJudge/UVA/299_UVA_299.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: UVA_299
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (UVA)
+ * Problem: 299
+ * Run ID: 70556443
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for UVA - 299
