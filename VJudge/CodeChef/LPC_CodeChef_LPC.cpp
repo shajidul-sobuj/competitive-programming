4418d586e031e0ca795da7918193bee933875c51
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:57:28 +0600

    [AC] VJudge CodeChefLPC - CodeChef_LPC

---
+++ VJudge/CodeChef/LPC_CodeChef_LPC.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_LPC
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: LPC
+ * Run ID: 70568627
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - LPC
