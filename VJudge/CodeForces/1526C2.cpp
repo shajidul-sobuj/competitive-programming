beddc7e92c8e05baab1b939059e97d6c6529db01
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 21:11:26 +0600

    [AC] VJudge CodeForces1526C2 - 1526C2

---
+++ VJudge/CodeForces/1526C2.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: 1526C2
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 1526C2
+ * Run ID: 71409882
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 1526C2
