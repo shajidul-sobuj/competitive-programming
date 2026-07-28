592ae0ab8e786b7c92e92a0590d4c0b8c17b4d40
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:53:08 +0600

    [AC] VJudge CodeForces985A - CodeForces_985A

---
+++ VJudge/CodeForces/985A_CodeForces_985A.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_985A
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 985A
+ * Run ID: 70858680
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 985A
