c2be9f52d92ecc0ce9ba015d308c30e998b0a995
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:52:08 +0600

    [AC] VJudge Tophadd-them-up - Toph_add-them-up

---
+++ VJudge/Toph/add-them-up_Toph_add-them-up.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: Toph_add-them-up
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (Toph)
+ * Problem: add-them-up
+ * Run ID: 71088941
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Toph - add-them-up
