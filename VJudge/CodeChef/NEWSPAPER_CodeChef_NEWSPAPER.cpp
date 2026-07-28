9c62d25894371deb683f038fa6b3bd5d4fe5b995
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:52:23 +0600

    [AC] VJudge CodeChefNEWSPAPER - CodeChef_NEWSPAPER

---
+++ VJudge/CodeChef/NEWSPAPER_CodeChef_NEWSPAPER.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_NEWSPAPER
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: NEWSPAPER
+ * Run ID: 71088874
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - NEWSPAPER
