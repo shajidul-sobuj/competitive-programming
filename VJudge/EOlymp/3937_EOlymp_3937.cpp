9e72867a639f489da0f3aeee5d28926c1ab8b20e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:56:54 +0600

    [AC] VJudge EOlymp3937 - EOlymp_3937

---
+++ VJudge/EOlymp/3937_EOlymp_3937.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_3937
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 3937
+ * Run ID: 70575418
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 3937
