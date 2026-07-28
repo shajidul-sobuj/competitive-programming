11b97e19d458ec8d5c78eee0eb9d75f1367ae6f6
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:23 +0600

    [AC] VJudge EOlymp972 - EOlymp_972

---
+++ VJudge/EOlymp/972_EOlymp_972.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_972
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 972
+ * Run ID: 70567118
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 972
