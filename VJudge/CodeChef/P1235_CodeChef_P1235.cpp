c6143b417cd3bfb57401798c940f98ed5e315b94
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:55:09 +0600

    [AC] VJudge CodeChefP1235 - CodeChef_P1235

---
+++ VJudge/CodeChef/P1235_CodeChef_P1235.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_P1235
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: P1235
+ * Run ID: 70809476
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - P1235
