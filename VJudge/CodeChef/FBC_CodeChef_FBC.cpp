2939b4626614920c7160f2d5e849f315d32418c4
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:51:23 +0600

    [AC] VJudge CodeChefFBC - CodeChef_FBC

---
+++ VJudge/CodeChef/FBC_CodeChef_FBC.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_FBC
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: FBC
+ * Run ID: 71089261
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - FBC
