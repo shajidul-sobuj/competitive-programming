2e9814e45d1920eff100a75e1c4121eb9e5837b6
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:55:16 +0600

    [AC] VJudge CodeChefP1209 - CodeChef_P1209

---
+++ VJudge/CodeChef/P1209_CodeChef_P1209.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_P1209
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: P1209
+ * Run ID: 70809404
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - P1209
