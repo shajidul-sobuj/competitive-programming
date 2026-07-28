8335d63186b6051d412e1574db374f4c9bfadc95
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:52:44 +0600

    [AC] VJudge CodeForces112A - CodeForces_112A

---
+++ VJudge/CodeForces/112A_CodeForces_112A.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_112A
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 112A
+ * Run ID: 70988740
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 112A
