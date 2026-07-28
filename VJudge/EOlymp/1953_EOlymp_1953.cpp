dea4c1f9b4bdcbc9d73f90a76ed1ce82e1e3bc7a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:57:13 +0600

    [AC] VJudge EOlymp1953 - EOlymp_1953

---
+++ VJudge/EOlymp/1953_EOlymp_1953.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_1953
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 1953
+ * Run ID: 70570719
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 1953
