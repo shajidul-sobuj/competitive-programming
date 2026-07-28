ed65d20d28c71270c194d27ea792d61ca56fab4c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:57:53 +0600

    [AC] VJudge CodeChefBINPARITY - CodeChef_BINPARITY

---
+++ VJudge/CodeChef/BINPARITY_CodeChef_BINPARITY.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_BINPARITY
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: BINPARITY
+ * Run ID: 70567971
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - BINPARITY
