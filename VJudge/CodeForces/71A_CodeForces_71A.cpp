5a7502e7673aad37352cdbd98d04f8f1cd73b171
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:52:29 +0600

    [AC] VJudge CodeForces71A - CodeForces_71A

---
+++ VJudge/CodeForces/71A_CodeForces_71A.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_71A
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 71A
+ * Run ID: 70989316
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 71A
