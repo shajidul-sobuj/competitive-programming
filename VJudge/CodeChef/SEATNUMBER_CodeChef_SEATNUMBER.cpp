859ec86b743bedd93f4141b72c52eaf2ddc708d3
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:51:31 +0600

    [AC] VJudge CodeChefSEATNUMBER - CodeChef_SEATNUMBER

---
+++ VJudge/CodeChef/SEATNUMBER_CodeChef_SEATNUMBER.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_SEATNUMBER
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: SEATNUMBER
+ * Run ID: 71089223
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - SEATNUMBER
