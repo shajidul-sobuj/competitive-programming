25f6680ef9313ffa988004edf4e9a7175c28ed8d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:00 +0600

    [AC] VJudge CodeChefMINWAGE - CodeChef_MINWAGE

---
+++ VJudge/CodeChef/MINWAGE_CodeChef_MINWAGE.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_MINWAGE
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: MINWAGE
+ * Run ID: 70567867
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - MINWAGE
