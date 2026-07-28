3d628148f350ec484cdf692c84145b1fd05468ed
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:08 +0600

    [AC] VJudge Tophn-th-prime - Toph_n-th-prime

---
+++ VJudge/Toph/n-th-prime_Toph_n-th-prime.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: Toph_n-th-prime
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (Toph)
+ * Problem: n-th-prime
+ * Run ID: 70567657
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Toph - n-th-prime
