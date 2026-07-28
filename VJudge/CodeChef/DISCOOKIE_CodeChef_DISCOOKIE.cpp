6f52be51187fa02e102523b2fe18b4139736bb39
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:53:37 +0600

    [AC] VJudge CodeChefDISCOOKIE - CodeChef_DISCOOKIE

---
+++ VJudge/CodeChef/DISCOOKIE_CodeChef_DISCOOKIE.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_DISCOOKIE
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: DISCOOKIE
+ * Run ID: 70810887
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - DISCOOKIE
