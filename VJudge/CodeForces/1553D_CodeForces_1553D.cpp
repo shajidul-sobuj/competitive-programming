b4cff0e4b1adf39374c055d6ff6ea7bd6c30c1d1
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:50:04 +0600

    [AC] VJudge CodeForces1553D - CodeForces_1553D

---
+++ VJudge/CodeForces/1553D_CodeForces_1553D.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_1553D
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 1553D
+ * Run ID: 71408326
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 1553D
