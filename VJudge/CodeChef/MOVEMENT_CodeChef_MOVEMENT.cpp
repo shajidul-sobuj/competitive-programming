f8e2a220aaf417372db62d21776e39582796efd7
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:54:54 +0600

    [AC] VJudge CodeChefMOVEMENT - CodeChef_MOVEMENT

---
+++ VJudge/CodeChef/MOVEMENT_CodeChef_MOVEMENT.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_MOVEMENT
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: MOVEMENT
+ * Run ID: 70809827
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - MOVEMENT
