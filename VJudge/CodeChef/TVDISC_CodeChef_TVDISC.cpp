aa3b5907155869e7c79023571166cd3731044ce9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:51:18 +0600

    [AC] VJudge CodeChefTVDISC - CodeChef_TVDISC

---
+++ VJudge/CodeChef/TVDISC_CodeChef_TVDISC.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_TVDISC
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: TVDISC
+ * Run ID: 71089321
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - TVDISC
