445e3ca0718c27e44217e62480ee7f0771ac4d2f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 21:12:33 +0600

    [AC] VJudge CodeChefMYSERVE - MYSERVE

---
+++ VJudge/CodeChef/MYSERVE.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: MYSERVE
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: MYSERVE
+ * Run ID: 71330586
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - MYSERVE
