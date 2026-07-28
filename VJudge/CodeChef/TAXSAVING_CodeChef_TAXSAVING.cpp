43fe48114bf72e4162d464c7b530b83832822123
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:52:01 +0600

    [AC] VJudge CodeChefTAXSAVING - CodeChef_TAXSAVING

---
+++ VJudge/CodeChef/TAXSAVING_CodeChef_TAXSAVING.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_TAXSAVING
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: TAXSAVING
+ * Run ID: 71089005
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - TAXSAVING
