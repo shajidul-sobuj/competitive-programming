d58fdc5c5033d8c75109f6976799152e9993b698
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:54:31 +0600

    [AC] VJudge CodeChefADVANCE - CodeChef_ADVANCE

---
+++ VJudge/CodeChef/ADVANCE_CodeChef_ADVANCE.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_ADVANCE
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: ADVANCE
+ * Run ID: 70810109
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - ADVANCE
