d4581e258b730d623d054abf2a99f956c3cff6fb
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:59:00 +0600

    [AC] VJudge EOlymp10246 - EOlymp_10246

---
+++ VJudge/EOlymp/10246_EOlymp_10246.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_10246
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 10246
+ * Run ID: 70554625
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 10246
