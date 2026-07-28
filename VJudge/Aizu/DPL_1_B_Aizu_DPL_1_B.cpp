2bb38a00401980bff7238e117215be0de803df1e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:56:05 +0600

    [AC] VJudge AizuDPL_1_B - Aizu_DPL_1_B

---
+++ VJudge/Aizu/DPL_1_B_Aizu_DPL_1_B.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: Aizu_DPL_1_B
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (Aizu)
+ * Problem: DPL_1_B
+ * Run ID: 70633159
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for Aizu - DPL_1_B
