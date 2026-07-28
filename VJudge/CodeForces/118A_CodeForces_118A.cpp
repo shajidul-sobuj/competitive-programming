a93a5b9c245c100cee9cdf474e32407874ae95f9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:50:12 +0600

    [AC] VJudge CodeForces118A - CodeForces_118A

---
+++ VJudge/CodeForces/118A_CodeForces_118A.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_118A
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 118A
+ * Run ID: 71407914
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 118A
