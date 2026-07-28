43a4757a48d3a62f15926a939cac9d4919ad4dba
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:54:38 +0600

    [AC] VJudge CodeChefTV - CodeChef_TV

---
+++ VJudge/CodeChef/TV_CodeChef_TV.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_TV
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: TV
+ * Run ID: 70810014
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - TV
