9d2c5775fc46cc00dbcae1547b67d77556693b0a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:31 +0600

    [AC] VJudge SPOJLIS2 - SPOJ_LIS2

---
+++ VJudge/SPOJ/LIS2_SPOJ_LIS2.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: SPOJ_LIS2
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (SPOJ)
+ * Problem: LIS2
+ * Run ID: 70567071
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for SPOJ - LIS2
