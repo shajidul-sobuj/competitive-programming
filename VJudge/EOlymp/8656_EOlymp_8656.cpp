5b039de78eb16227a0a36c75e14141d1032eb646
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:56:25 +0600

    [AC] VJudge EOlymp8656 - EOlymp_8656

---
+++ VJudge/EOlymp/8656_EOlymp_8656.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_8656
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 8656
+ * Run ID: 70576653
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 8656
