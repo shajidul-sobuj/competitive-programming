704de2a2055faad7e40c503f99a6b90fd8fbf2ad
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:57:02 +0600

    [AC] VJudge EOlymp1462 - EOlymp_1462

---
+++ VJudge/EOlymp/1462_EOlymp_1462.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_1462
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 1462
+ * Run ID: 70572726
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 1462
