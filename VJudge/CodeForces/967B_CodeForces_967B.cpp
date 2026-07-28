a5177042e8f184bb7370828bf0cb01052e4fe78d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:53:00 +0600

    [AC] VJudge CodeForces967B - CodeForces_967B

---
+++ VJudge/CodeForces/967B_CodeForces_967B.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_967B
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 967B
+ * Run ID: 70859530
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 967B
