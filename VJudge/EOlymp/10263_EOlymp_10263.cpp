f7578a5d928fbeca1c1ba0754e804931ff6f7a95
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:15 +0600

    [AC] VJudge EOlymp10263 - EOlymp_10263

---
+++ VJudge/EOlymp/10263_EOlymp_10263.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: EOlymp_10263
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (EOlymp)
+ * Problem: 10263
+ * Run ID: 70567268
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for EOlymp - 10263
