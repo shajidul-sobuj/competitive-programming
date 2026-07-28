86cf05eaf36a238b9da780a9a1286d1085704ae4
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:51:46 +0600

    [AC] VJudge CodeChefREACHTARGET - CodeChef_REACHTARGET

---
+++ VJudge/CodeChef/REACHTARGET_CodeChef_REACHTARGET.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_REACHTARGET
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: REACHTARGET
+ * Run ID: 71089118
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - REACHTARGET
