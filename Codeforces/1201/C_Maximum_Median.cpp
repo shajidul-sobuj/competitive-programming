4a6da62d0c912dac781ef60e2c241528cbad9710
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-29 02:26:19 +0600

    [SyncForge] Auto-push Codeforces: Maximum Median

---
+++ Codeforces/1201/C_Maximum_Median.cpp
@@ -0,0 +1,12 @@
+// Codeforces Solution: Maximum Median
+// Contest/Problem ID: 1201 C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Engine
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
