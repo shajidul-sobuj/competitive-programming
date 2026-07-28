0ba0f127cc6111ecd93e97fbc40598a50212bb93
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:58:44 +0600

    [AC] VJudge UVA12541 - UVA_12541

---
+++ VJudge/UVA/12541_UVA_12541.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: UVA_12541
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (UVA)
+ * Problem: 12541
+ * Run ID: 70556502
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for UVA - 12541
