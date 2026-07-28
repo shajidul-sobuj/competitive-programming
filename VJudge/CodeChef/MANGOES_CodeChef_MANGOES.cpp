2e7a0ac2128e22d662e0bf594a392ac33d6165f7
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:54:25 +0600

    [AC] VJudge CodeChefMANGOES - CodeChef_MANGOES

---
+++ VJudge/CodeChef/MANGOES_CodeChef_MANGOES.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_MANGOES
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: MANGOES
+ * Run ID: 70810182
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - MANGOES
