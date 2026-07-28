d65d6e82cbb95483fbba4dc8f7b3c21c655aa198
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 21:12:11 +0600

    [AC] VJudge CodeChefADDPOS - ADDPOS

---
+++ VJudge/CodeChef/ADDPOS.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: ADDPOS
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: ADDPOS
+ * Run ID: 71330856
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - ADDPOS
