2d08f0c8b1b09d254ea0b824fdb8df62a65349e1
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:50:55 +0600

    [AC] VJudge CodeForces231A - CodeForces_231A

---
+++ VJudge/CodeForces/231A_CodeForces_231A.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeForces_231A
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeForces)
+ * Problem: 231A
+ * Run ID: 71142818
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeForces - 231A
