c9412afb1e59d1dd71b729eaa51baa011c5c9424
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:57:21 +0600

    [AC] VJudge CodeChefNPRPE - CodeChef_NPRPE

---
+++ VJudge/CodeChef/NPRPE_CodeChef_NPRPE.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_NPRPE
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: NPRPE
+ * Run ID: 70570686
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - NPRPE
