265dad95591b462c0260f2698389f2705f9196e7
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:55:24 +0600

    [AC] VJudge AtCoderdp_b - AtCoder_dp_b

---
+++ VJudge/AtCoder/dp_b_AtCoder_dp_b.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: AtCoder_dp_b
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (AtCoder)
+ * Problem: dp_b
+ * Run ID: 70685470
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for AtCoder - dp_b
