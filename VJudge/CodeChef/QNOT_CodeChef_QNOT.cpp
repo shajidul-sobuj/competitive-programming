8afa717380028200c5222c318fe5093fbcf663b5
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:54:00 +0600

    [AC] VJudge CodeChefQNOT - CodeChef_QNOT

---
+++ VJudge/CodeChef/QNOT_CodeChef_QNOT.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_QNOT
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: QNOT
+ * Run ID: 70810598
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - QNOT
