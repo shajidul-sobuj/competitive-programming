10b128de369578c5bb21110653eeae6844cc621d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-28 14:51:53 +0600

    [AC] VJudge CodeChefDOREWARD - CodeChef_DOREWARD

---
+++ VJudge/CodeChef/DOREWARD_CodeChef_DOREWARD.cpp
@@ -0,0 +1,14 @@
+/**
+ * Platform: VJudge
+ * Problem: CodeChef_DOREWARD
+ * Synced via CF Auto Push
+ */
+
+/**
+ * Platform: VJudge (CodeChef)
+ * Problem: DOREWARD
+ * Run ID: 71089067
+ * Synced via CF Auto Push
+ */
+
+// Accepted Solution for CodeChef - DOREWARD
