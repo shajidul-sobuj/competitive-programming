a5eb11cecb57ca5ca9397167c491ceee908c2410
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 11:21:14 +0600

    [SyncForge] Auto-push LeetCode: Binary Tree Postorder Traversal

---
+++ LeetCode/LeetCode/binary-tree-postorder-traversal_Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,12 @@
+// LeetCode Solution: Binary Tree Postorder Traversal
+// Contest/Problem ID: LeetCode binary-tree-postorder-traversal
+// Language: cpp
+// Synced via SyncForge Engine
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
