ea475fefb5e895d8db70944bcfa36c5770878d85
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:56:28 +0600

    [SyncForge] Auto-push Codeforces: Money Trees

---
+++ Codeforces/1873/F - Money_Trees.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Money Trees
+// Contest: 1873
+// URL: https://codeforces.com/contest/1873/problem/F
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:56:27 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
