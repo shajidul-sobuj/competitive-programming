62296b8b7dcc73f35a9317cc09a980c943c1d428
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:56:21 +0600

    [SyncForge] Auto-push Codeforces: Boats Competition

---
+++ Codeforces/1399/C - Boats_Competition.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Boats Competition
+// Contest: 1399
+// URL: https://codeforces.com/contest/1399/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:56:20 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
