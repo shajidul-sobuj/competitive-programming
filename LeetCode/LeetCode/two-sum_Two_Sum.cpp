a0942011359b94fd65038d8c6db7ee987a7be60b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 10:55:57 +0600

    [SyncForge] Auto-push LeetCode: Two Sum

---
+++ LeetCode/LeetCode/two-sum_Two_Sum.cpp
@@ -0,0 +1,12 @@
+// LeetCode Solution: Two Sum
+// Contest/Problem ID: LeetCode two-sum
+// Language: cpp
+// Synced via SyncForge Engine
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
