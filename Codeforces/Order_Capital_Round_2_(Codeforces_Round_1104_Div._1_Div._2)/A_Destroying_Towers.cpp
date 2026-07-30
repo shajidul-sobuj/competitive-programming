0257127520418a4fb2f214ba98a23a87cbad9c58
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:11:43 +0600

    [SyncForge] Auto-push Codeforces: Destroying Towers

---
+++ Codeforces/Order_Capital_Round_2_(Codeforces_Round_1104_Div._1_Div._2)/A_Destroying_Towers.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Destroying Towers
+// Contest: 2237
+// URL: https://codeforces.com/contest/2237/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:11:43 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
