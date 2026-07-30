57493d3cfe6f83118eb7aede19ef32f8dd4702b1
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:54:12 +0600

    [SyncForge] Auto-push Codeforces: Students in Railway Carriage

---
+++ Codeforces/962/B - Students_in_Railway_Carriage.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Students in Railway Carriage
+// Contest: 962
+// URL: https://codeforces.com/contest/962/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:54:12 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
