55f4e218bcec730b1a10cd042df6422270d6c9ae
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:21:02 +0600

    [SyncForge] Auto-push AtCoder: arc087 a

---
+++ AtCoder/Contest_ARC087/A_arc087_a.cpp
@@ -0,0 +1,42 @@
+/**
+ * Platform: AtCoder
+ * Problem: arc087 a
+ * Contest: ARC087
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n;
+    cin >> n;
+    map<int, int> fr;
+    while(n--)
+    {
+        int x;
+        cin >> x;
+        fr[x]++;
+    }
+    ll count = 0;
+    for (auto [x,y] : fr)
+    {
+        // cout << x << " -> " << y << endl;
+        if (x > y)
+        {
+            count += y;
+        }else{
+            count += y-x;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
