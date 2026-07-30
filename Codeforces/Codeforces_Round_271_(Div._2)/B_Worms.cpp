437cb06e7b18722df04b8fa5de6791f13688ea2d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:10 +0600

    [SyncForge] Auto-push Codeforces: Worms

---
+++ Codeforces/Codeforces_Round_271_(Div._2)/B_Worms.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Worms
+// Contest: 474
+// URL: https://codeforces.com/contest/474/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:09 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
