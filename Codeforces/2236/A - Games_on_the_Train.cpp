90cf54d86ebcdcc81ec77b3ad2bf6bb95131597e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:55:14 +0600

    [SyncForge] Auto-push Codeforces: Games on the Train

---
+++ Codeforces/2236/A - Games_on_the_Train.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Games on the Train
+// Contest: 2236
+// URL: https://codeforces.com/contest/2236/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:55:13 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
