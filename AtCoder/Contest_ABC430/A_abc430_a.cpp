b3a9a40f9a3d91dcde535c8a13a30473aea75493
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:18:28 +0600

    [SyncForge] Auto-push AtCoder: abc430 a

---
+++ AtCoder/Contest_ABC430/A_abc430_a.cpp
@@ -0,0 +1,23 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc430 a
+ * Contest: ABC430
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if (c>=a && d<b){
+        printf("Yes\n");
+    }else {
+        printf("No\n");
+    }
+    return 0;
+}
+
+
+
