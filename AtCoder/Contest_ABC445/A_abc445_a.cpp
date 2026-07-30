3f04235a68a89ce87e3ff4f83f04fa23d67a140a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:20:03 +0600

    [SyncForge] Auto-push AtCoder: abc445 a

---
+++ AtCoder/Contest_ABC445/A_abc445_a.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc445 a
+ * Contest: ABC445
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[0]==s[s.size()-1])
+    {
+        cout << "Yes\n";
+    }else {
+        cout << "No\n";
+    }
+    return 0;
+}
