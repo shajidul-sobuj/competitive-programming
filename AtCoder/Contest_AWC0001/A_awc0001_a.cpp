fce7dc81f493d544826d9ff0bdb590ff224219c9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:19:45 +0600

    [SyncForge] Auto-push AtCoder: awc0001 a

---
+++ AtCoder/Contest_AWC0001/A_awc0001_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Problem: awc0001 a
+ * Contest: AWC0001
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n;
+    cin >> n;
+    cout << (n+1) << endl;
+    return 0;
+}
