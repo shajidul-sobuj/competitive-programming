f4ed160ef20cbfd199bb1d8c6286502cbf589082
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:53:56 +0600

    [SyncForge] Auto-push Codeforces: farmpiggie and Subset Sum

---
+++ Codeforces/2246/A - farmpiggie_and_Subset_Sum.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: farmpiggie and Subset Sum
+// Contest: 2246
+// URL: https://codeforces.com/contest/2246/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:53:56 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
