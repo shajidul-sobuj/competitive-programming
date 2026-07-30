b0a84b9c67965782a2e821608a506bf4bdbdaedf
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:06 +0600

    [SyncForge] Auto-push Codeforces: Word Game

---
+++ Codeforces/Codeforces_Round_817_(Div._4)/C_Word_Game.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Word Game
+// Contest: 1722
+// URL: https://codeforces.com/contest/1722/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:05 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
