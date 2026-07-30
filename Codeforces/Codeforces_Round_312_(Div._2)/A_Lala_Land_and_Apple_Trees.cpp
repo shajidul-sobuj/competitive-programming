8b905f9e69d5bc045875d4b571a13547c261ba73
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:13 +0600

    [SyncForge] Auto-push Codeforces: Lala Land and Apple Trees

---
+++ Codeforces/Codeforces_Round_312_(Div._2)/A_Lala_Land_and_Apple_Trees.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Lala Land and Apple Trees
+// Contest: 558
+// URL: https://codeforces.com/contest/558/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:13 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
