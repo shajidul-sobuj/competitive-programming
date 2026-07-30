f997d2242473beb4a8451e00d346ab05ab68d00c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:14:42 +0600

    [SyncForge] Auto-push Codeforces: Polycarp Training

---
+++ Codeforces/Codeforces_Round_560_(Div._3)/B_Polycarp_Training.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Polycarp Training
+// Contest: 1165
+// URL: https://codeforces.com/contest/1165/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:14:41 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
