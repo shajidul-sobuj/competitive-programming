6f55763548d16e21005164c7712442bba5116bca
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:20:29 +0600

    [SyncForge] Auto-push AtCoder: abc125 a

---
+++ AtCoder/Contest_ABC125/A_abc125_a.cpp
@@ -0,0 +1,19 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc125 a
+ * Contest: ABC125
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,k;
+    cin >> a >> b >> k;
+    k = k+0.5;
+    int ans = (k/a)*b;
+    cout << (int)ans << endl;
+    return 0;
+}
