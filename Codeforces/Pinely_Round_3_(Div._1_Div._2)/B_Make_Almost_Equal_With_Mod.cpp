ec34378c0b137c366bfddd3a7fdf20ab2d119938
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:12:55 +0600

    [SyncForge] Auto-push Codeforces: Make Almost Equal With Mod

---
+++ Codeforces/Pinely_Round_3_(Div._1_Div._2)/B_Make_Almost_Equal_With_Mod.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Make Almost Equal With Mod
+// Contest: 1909
+// URL: https://codeforces.com/contest/1909/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:12:55 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
