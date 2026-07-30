5e562f283b9fd009d071c3612d7a71e2deac752d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:13:11 +0600

    [SyncForge] Auto-push Codeforces: Even Subset Sum Problem

---
+++ Codeforces/Codeforces_Round_626_(Div._2_based_on_Moscow_Open_Olympiad_in_Informatics)/A_Even_Subset_Sum_Problem.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Even Subset Sum Problem
+// Contest: 1323
+// URL: https://codeforces.com/contest/1323/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:13:11 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
