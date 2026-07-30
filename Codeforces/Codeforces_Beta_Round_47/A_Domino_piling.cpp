b849c1e03f010c593a1664963c33a725c9f83d0f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:17:59 +0600

    [SyncForge] Auto-push Codeforces: Domino piling

---
+++ Codeforces/Codeforces_Beta_Round_47/A_Domino_piling.cpp
@@ -0,0 +1,18 @@
+/**
+ * Platform: Codeforces
+ * Problem: Domino piling
+ * Contest: 50
+ * URL: https://codeforces.com/contest/50/problem/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int ans = (n*m)/2;
+    cout << ans << endl;
+    return 0;
+}
