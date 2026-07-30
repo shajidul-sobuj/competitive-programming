140926fb7d1a5804e9212dc9d5817e0dd5775af6
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:13:03 +0600

    [SyncForge] Auto-push Codeforces: Counterexample

---
+++ Codeforces/Codeforces_Round_275_(Div._2)/A_Counterexample_.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Counterexample 
+// Contest: 483
+// URL: https://codeforces.com/contest/483/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:13:03 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
