53f83af39a4c5f50e88bb206b4521234ed08fb19
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:54:28 +0600

    [SyncForge] Auto-push Codeforces: Businessmen Problems

---
+++ Codeforces/981/B - Businessmen_Problems.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Businessmen Problems
+// Contest: 981
+// URL: https://codeforces.com/contest/981/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:54:28 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
