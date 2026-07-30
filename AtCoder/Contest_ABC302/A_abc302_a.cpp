a8e8e4bfc736e63ddeecd0dcac21176701c697fa
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:18:45 +0600

    [SyncForge] Auto-push AtCoder: abc302 a

---
+++ AtCoder/Contest_ABC302/A_abc302_a.cpp
@@ -0,0 +1,21 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc302 a
+ * Contest: ABC302
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a,b;
+    cin >> a >> b;
+    if (a%b==0) {
+        cout << a/b << endl;
+    }else {
+        cout << (a/b)+1 << endl;
+    }
+    return 0;
+}
