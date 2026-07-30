7fa2d9eca478bd56585bd83695b68e597696c171
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:19:41 +0600

    [SyncForge] Auto-push AtCoder: abc443 a

---
+++ AtCoder/Contest_ABC443/A_abc443_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc443 a
+ * Contest: ABC443
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s << "s" << endl;
+    return 0;
+}
