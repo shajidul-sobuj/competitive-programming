8e0502b4a29eb1d9eed0dc84f2ebd88f04fe9fc2
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 10:09:14 +0600

    [SyncForge] Auto-push AtCoder: abc149 b

--- AtCoder/ABC149/B_abc149_b.cpp
+++ AtCoder/ABC149/B_abc149_b.cpp
@@ -1,26 +1,12 @@
-/**
- * Platform: AtCoder
- * Contest: ABC149
- * Problem: B - abc149 b
- * Language: C++23 (GCC 15.2.0)
- * Date: 2026-07-26 21:34:32
- */
+// AtCoder Solution: abc149 b
+// Contest/Problem ID: ABC149 B
+// Language: C++23 (GCC 15.2.0)
+// Synced via SyncForge Engine
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    long long a, b, k;
-    cin >> a >> b >> k;
-
-    if (k <= a) {
-        a -= k;
-    } else {
-        k -= a;
-        a = 0;
-        b = max(0LL, b - k);
-    }
-
-    cout << a << " " << b << endl;
+    // Solution code synced automatically
     return 0;
 }
