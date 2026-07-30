a3091efdbdd754ea1f2a330ce477bb673c62ab99
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:20:20 +0600

    [SyncForge] Auto-push AtCoder: abc172 b

---
+++ AtCoder/Contest_ABC172/B_abc172_b.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc172 b
+ * Contest: ABC172
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s,t;
+    cin >> s >> t;
+    int count = 0;
+    for (int i=0;i<s.size();i++)
+    {
+        if (s[i]!=t[i]) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
