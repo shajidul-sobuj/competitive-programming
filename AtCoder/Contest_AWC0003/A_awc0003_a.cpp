fb34b47e48e7b39717a3ac28019ca4f54ccd5ec7
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:19:59 +0600

    [SyncForge] Auto-push AtCoder: awc0003 a

---
+++ AtCoder/Contest_AWC0003/A_awc0003_a.cpp
@@ -0,0 +1,24 @@
+/**
+ * Platform: AtCoder
+ * Problem: awc0003 a
+ * Contest: AWC0003
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n,k;
+    cin >> n >> k;
+    long long int count = 0;
+    while(n--)
+    {
+        long long int x,y;
+        cin >> x >> y;
+        if (x*y>=k) count++;
+    }
+    cout << count << endl;
+    return 0;
+}
