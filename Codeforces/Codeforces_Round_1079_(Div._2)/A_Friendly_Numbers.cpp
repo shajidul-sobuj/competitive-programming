9f6e7633bb4b2e9c5a431342588f525632cb7af0
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:17:50 +0600

    [SyncForge] Auto-push Codeforces: Friendly Numbers

---
+++ Codeforces/Codeforces_Round_1079_(Div._2)/A_Friendly_Numbers.cpp
@@ -0,0 +1,39 @@
+/**
+ * Platform: Codeforces
+ * Problem: Friendly Numbers
+ * Contest: 2197
+ * URL: https://codeforces.com/contest/2197/problem/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int ans(long long int n) {
+    long long int sum = 0;
+    while (n>0) {
+        sum += (n%10);
+        n = n/10;
+    }
+    return sum;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long int x;
+        cin >> x;
+
+        int count = 0;
+        for (long long int y=x+1;y<=x+81;y++) {
+            if (y-ans(y) == x) {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
