92887937671d656d351f7246502f82918255f77e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:11:52 +0600

    [SyncForge] Auto-push Codeforces: Deranged Deletions

---
+++ Codeforces/EPIC_Institute_of_Technology_Round_Summer_2025_(Codeforces_Round_1036_Div._1_Div._2)/A_Deranged_Deletions.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Deranged Deletions
+// Contest: 2124
+// URL: https://codeforces.com/contest/2124/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:11:51 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
