43d24ccbf15f038b7ac81036d5ad2411562f458c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:20:10 +0600

    [SyncForge] Auto-push AtCoder: abc445 c

---
+++ AtCoder/Contest_ABC445/C_abc445_c.cpp
@@ -0,0 +1,29 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc445 c
+ * Contest: ABC445
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> v(n+1);
+    for (int i=1;i<=n;i++) cin >> v[i];
+    for (int i=1;i<=n;i++)
+    {
+        int idx = i;
+
+        while (idx != v[idx]) {
+            v[idx] = v[v[idx]];
+            idx = v[idx];
+        }
+
+        cout << idx << " ";
+    }
+    return 0;
+}
