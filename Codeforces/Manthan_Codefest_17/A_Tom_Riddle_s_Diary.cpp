3a52ebc9bb532ef2c5dd4e5f959e830c9e931d6d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:55 +0600

    [SyncForge] Auto-push Codeforces: Tom Riddle's Diary

---
+++ Codeforces/Manthan_Codefest_17/A_Tom_Riddle_s_Diary.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Tom Riddle's Diary
+// Contest: 855
+// URL: https://codeforces.com/contest/855/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:55 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
