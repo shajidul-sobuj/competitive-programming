234646cfbad6f2e0f5596aca54130720ccc50d99
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:23 +0600

    [SyncForge] Auto-push Codeforces: Misha and Changing Handles

---
+++ Codeforces/Codeforces_Round_285_(Div._2)/B_Misha_and_Changing_Handles.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Misha and Changing Handles
+// Contest: 501
+// URL: https://codeforces.com/contest/501/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:22 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
