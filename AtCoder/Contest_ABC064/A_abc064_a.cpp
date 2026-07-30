e696e1b68c7337061978d222c646be9126d333d6
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:20:47 +0600

    [SyncForge] Auto-push AtCoder: abc064 a

---
+++ AtCoder/Contest_ABC064/A_abc064_a.cpp
@@ -0,0 +1,20 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc064 a
+ * Contest: ABC064
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    long long x = c+(b*10)+(a*100);
+    if (x%4==0) cout << "YES\n";
+    else cout << "NO\n";
+    // cout << x << endl;
+    return 0;
+}
