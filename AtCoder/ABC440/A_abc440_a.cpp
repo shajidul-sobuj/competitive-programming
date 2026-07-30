7e9c3ed70635a1f0cad48c41ee82a1f16aa4c79d
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 10:09:19 +0600

    [SyncForge] Auto-push AtCoder: abc440 a

--- AtCoder/ABC440/A_abc440_a.cpp
+++ AtCoder/ABC440/A_abc440_a.cpp
@@ -1,17 +1,12 @@
-/**
- * Platform: AtCoder
- * Contest: ABC440
- * Problem: A - abc440 a
- * Language: C++23 (GCC 15.2.0)
- * Date: 2026-07-26 21:35:04
- */
+// AtCoder Solution: abc440 a
+// Contest/Problem ID: ABC440 A
+// Language: C++23 (GCC 15.2.0)
+// Synced via SyncForge Engine
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int x,y;
-    cin >> x >> y;
-    cout << (long long int)(x*(pow(2,y))) << endl;
+    // Solution code synced automatically
     return 0;
 }
