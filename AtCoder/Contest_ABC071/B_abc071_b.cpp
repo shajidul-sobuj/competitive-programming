07b437ee00fe619244ea2e3cc3ae404d7461beb8
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:20:17 +0600

    [SyncForge] Auto-push AtCoder: abc071 b

---
+++ AtCoder/Contest_ABC071/B_abc071_b.cpp
@@ -0,0 +1,32 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc071 b
+ * Contest: ABC071
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    vector <int> fr(26,0);
+    for (int i=0;i<s.size();i++)
+    {
+        int idx = (int)s[i]-'a';
+        fr[idx]++;
+    }
+    for (int i=0;i<26;i++)
+    {
+        int x = fr[i];
+        if (x == 0)
+        {
+            cout << (char)(i+'a') << endl;
+            return 0;
+        }
+    }
+    cout << "None\n";
+    return 0;
+}
