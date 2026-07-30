5d982b79a5768c82c1ae18d44217fc693f3cc9cd
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:26 +0600

    [SyncForge] Auto-push Codeforces: Peacock Party

---
+++ Codeforces/Contest_103647/C_Peacock_Party.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Peacock Party
+// Contest: 103647
+// URL: https://codeforces.com/contest/103647/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:26 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
