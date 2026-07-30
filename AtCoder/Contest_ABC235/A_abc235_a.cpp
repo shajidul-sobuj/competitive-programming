0faf04e6fe2901e8b0dc6af2f5ffc269eb002741
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:19:02 +0600

    [SyncForge] Auto-push AtCoder: abc235 a

---
+++ AtCoder/Contest_ABC235/A_abc235_a.cpp
@@ -0,0 +1,17 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc235 a
+ * Contest: ABC235
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << 111*(s[0]-'0'+s[1]-'0'+s[2]-'0') << endl;
+    return 0;
+}
