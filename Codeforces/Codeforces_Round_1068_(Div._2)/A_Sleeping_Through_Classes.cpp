ba3408f6004c96182dc7c439ff0d257b31b8cf91
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:17:54 +0600

    [SyncForge] Auto-push Codeforces: Sleeping Through Classes

---
+++ Codeforces/Codeforces_Round_1068_(Div._2)/A_Sleeping_Through_Classes.cpp
@@ -0,0 +1,44 @@
+/**
+ * Platform: Codeforces
+ * Problem: Sleeping Through Classes
+ * Contest: 2173
+ * URL: https://codeforces.com/contest/2173/problem/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,k;
+    cin >> n >> k;
+    string v;
+    cin >> v;
+    int test = 0;
+    int count = 0;
+    for (int i=0;i<n;i++)
+    {
+        if (v[i] == '0' && test == 0 )
+        {
+            count++;
+        }else if (v[i] == '1')
+        {
+            test = k;
+        }else if (v[i] == '0' && test != 0 )
+        {
+            test--;
+        }
+    }
+    cout << count << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
