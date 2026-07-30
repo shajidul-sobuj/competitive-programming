aa52ba71d5b5878fc5ca996e6135e3a56515a916
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:14:05 +0600

    [SyncForge] Auto-push Codeforces: Right Maximum

---
+++ Codeforces/Educational_Codeforces_Round_188_(Rated_for_Div._2)/B_Right_Maximum.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Right Maximum
+// Contest: 2204
+// URL: https://codeforces.com/contest/2204/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:14:05 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
