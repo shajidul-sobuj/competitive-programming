c5e6ca1f20250302ff229925367fc9eb161a45e6
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:18:07 +0600

    [SyncForge] Auto-push Codeforces: Divisible Permutation

---
+++ Codeforces/Codeforces_Round_1077_(Div._2)/A_Divisible_Permutation.cpp
@@ -0,0 +1,40 @@
+/**
+ * Platform: Codeforces
+ * Problem: Divisible Permutation
+ * Contest: 2188
+ * URL: https://codeforces.com/contest/2188/problem/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i=n-2,val=n; i>=0; i=i-2,val--)
+    {
+        a[i] = val;
+    }
+    for (int i=n-1,val=1; i>=0; i=i-2,val++)
+    {
+        a[i] = val;
+    }
+    for (int x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
