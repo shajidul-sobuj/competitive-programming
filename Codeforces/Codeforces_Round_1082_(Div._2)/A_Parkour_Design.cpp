c1da14f74a8d8939b6fae01ab209bdc4371ac6d0
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:16:27 +0600

    [SyncForge] Auto-push Codeforces: Parkour Design

---
+++ Codeforces/Codeforces_Round_1082_(Div._2)/A_Parkour_Design.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Parkour Design
+// Contest: 2202
+// URL: https://codeforces.com/contest/2202/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:16:27 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
