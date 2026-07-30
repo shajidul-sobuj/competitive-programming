fdf817db749633dee3a4935b3c4b85a0bdae7ece
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:39 +0600

    [SyncForge] Auto-push Codeforces: Registration System

---
+++ Codeforces/Codeforces_Beta_Round_4_(Div._2_Only)/C_Registration_System.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Registration System
+// Contest: 4
+// URL: https://codeforces.com/contest/4/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:38 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
