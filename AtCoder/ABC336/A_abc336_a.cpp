6e66f0605e5015ac8c0be021a881ccb77cebb51f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 10:09:15 +0600

    [SyncForge] Auto-push AtCoder: abc336 a

--- AtCoder/ABC336/A_abc336_a.cpp
+++ AtCoder/ABC336/A_abc336_a.cpp
@@ -1,21 +1,12 @@
-/**
- * Platform: AtCoder
- * Contest: ABC336
- * Problem: A - abc336 a
- * Language: C++23 (GCC 15.2.0)
- * Date: 2026-07-26 21:34:41
- */
+// AtCoder Solution: abc336 a
+// Contest/Problem ID: ABC336 A
+// Language: C++23 (GCC 15.2.0)
+// Synced via SyncForge Engine
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    cout << "L";
-    while(n--){
-        cout << "o";
-    }
-    cout << "ng\n";
+    // Solution code synced automatically
     return 0;
 }
