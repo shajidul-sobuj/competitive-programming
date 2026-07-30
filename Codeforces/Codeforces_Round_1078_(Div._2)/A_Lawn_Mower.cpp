945ca5b45eac8471c1ae997d99c49455e6d56ecf
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:18:19 +0600

    [SyncForge] Auto-push Codeforces: Lawn Mower

---
+++ Codeforces/Codeforces_Round_1078_(Div._2)/A_Lawn_Mower.cpp
@@ -0,0 +1,23 @@
+/**
+ * Platform: Codeforces
+ * Problem: Lawn Mower
+ * Contest: 2194
+ * URL: https://codeforces.com/contest/2194/problem/A
+ * Synced via SyncForge Extension
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            long long n, w;
+            cin >> n >> w;
+            cout << n - (n / w) << "\n";
+        }
+    }
+    return 0;
+}
