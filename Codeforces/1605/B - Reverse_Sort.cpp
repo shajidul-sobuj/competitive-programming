1177fcff32c0ad700fe21518b3becab1a014cc09
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:56:17 +0600

    [SyncForge] Auto-push Codeforces: Reverse Sort

---
+++ Codeforces/1605/B - Reverse_Sort.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Reverse Sort
+// Contest: 1605
+// URL: https://codeforces.com/contest/1605/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:56:17 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
