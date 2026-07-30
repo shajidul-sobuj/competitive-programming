76fae55d15d40e8422954e92df3f499b287311fe
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:13:28 +0600

    [SyncForge] Auto-push Codeforces: Simply Sitting on Chairs

---
+++ Codeforces/Codeforces_Round_1089_(Div._2)/B_Simply_Sitting_on_Chairs.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Simply Sitting on Chairs
+// Contest: 2210
+// URL: https://codeforces.com/contest/2210/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:13:27 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
