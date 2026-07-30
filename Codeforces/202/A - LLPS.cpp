a366eade8fec2c08c854df0b7ecdca0ee63b3a9b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:56:03 +0600

    [SyncForge] Auto-push Codeforces: LLPS

---
+++ Codeforces/202/A - LLPS.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: LLPS
+// Contest: 202
+// URL: https://codeforces.com/contest/202/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:56:02 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
