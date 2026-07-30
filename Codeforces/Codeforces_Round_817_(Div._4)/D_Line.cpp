a602cc38daa26f734abb0819856429b2967e0614
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:15:02 +0600

    [SyncForge] Auto-push Codeforces: Line

---
+++ Codeforces/Codeforces_Round_817_(Div._4)/D_Line.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Line
+// Contest: 1722
+// URL: https://codeforces.com/contest/1722/problem/D
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:01 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
