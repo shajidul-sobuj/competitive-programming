456647255c966a87bce9cdf5e885a625e6dfa898
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:16:21 +0600

    [SyncForge] Auto-push Codeforces: Sereja and Dima

---
+++ Codeforces/Codeforces_Round_223_(Div._2)/A_Sereja_and_Dima.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Sereja and Dima
+// Contest: 381
+// URL: https://codeforces.com/contest/381/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:16:21 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
