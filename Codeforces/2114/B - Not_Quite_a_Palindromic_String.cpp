3f70e8552fef01d7773a2bb93fa6ab565bd70d5b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:55:44 +0600

    [SyncForge] Auto-push Codeforces: Not Quite a Palindromic String

---
+++ Codeforces/2114/B - Not_Quite_a_Palindromic_String.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Not Quite a Palindromic String
+// Contest: 2114
+// URL: https://codeforces.com/contest/2114/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:55:43 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
