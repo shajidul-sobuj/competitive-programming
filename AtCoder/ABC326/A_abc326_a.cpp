e83985d75752f6d58bfda58ac8ccf7e14a019b3c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 10:09:12 +0600

    [SyncForge] Auto-push AtCoder: abc326 a

--- AtCoder/ABC326/A_abc326_a.cpp
+++ AtCoder/ABC326/A_abc326_a.cpp
@@ -1,22 +1,12 @@
-/**
- * Platform: AtCoder
- * Contest: ABC326
- * Problem: A - abc326 a
- * Language: C++23 (GCC 15.2.0)
- * Date: 2026-07-26 21:34:24
- */
+// AtCoder Solution: abc326 a
+// Contest/Problem ID: ABC326 A
+// Language: C++23 (GCC 15.2.0)
+// Synced via SyncForge Engine
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int x,y;
-    cin >> x >> y;
-    int t = x-y;
-    if ((t > 0 && t <= 3) || ((t < 0 && t >= -2))) {
-        cout << "Yes\n";
-    }else {
-        cout << "No\n";
-    }
+    // Solution code synced automatically
     return 0;
 }
