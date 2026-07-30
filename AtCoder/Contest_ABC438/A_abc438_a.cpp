64b9a5b8972672c6dcc537f9a26b720bb4cdf80a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:18:35 +0600

    [SyncForge] Auto-push AtCoder: abc438 a

---
+++ AtCoder/Contest_ABC438/A_abc438_a.cpp
@@ -0,0 +1,22 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc438 a
+ * Contest: ABC438
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int d,f;
+    cin >> d >> f;
+    int offset = d % 7;
+    int n = f - offset;
+    if (n <= 0) {
+        n += 7;
+    }
+    cout << n << endl;
+    return 0;
+}
