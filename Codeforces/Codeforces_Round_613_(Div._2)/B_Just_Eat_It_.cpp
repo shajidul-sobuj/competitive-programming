e4aa356d4a48401a0d39acd0e19bb0faccbcc616
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:11:40 +0600

    [SyncForge] Auto-push Codeforces: Just Eat It!

---
+++ Codeforces/Codeforces_Round_613_(Div._2)/B_Just_Eat_It_.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Just Eat It!
+// Contest: 1285
+// URL: https://codeforces.com/contest/1285/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:11:39 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
