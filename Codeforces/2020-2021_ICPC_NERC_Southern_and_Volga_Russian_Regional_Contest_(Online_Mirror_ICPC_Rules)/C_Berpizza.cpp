13fa95303924d218d4e431f667c277ba2fd3a163
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:14:18 +0600

    [SyncForge] Auto-push Codeforces: Berpizza

---
+++ Codeforces/2020-2021_ICPC_NERC_Southern_and_Volga_Russian_Regional_Contest_(Online_Mirror_ICPC_Rules)/C_Berpizza.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Berpizza
+// Contest: 1468
+// URL: https://codeforces.com/contest/1468/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:14:17 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
