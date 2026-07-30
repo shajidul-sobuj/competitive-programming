5cd0fd9e4c196cd912e0f83b813fd9256398c83f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:11:32 +0600

    [SyncForge] Auto-push Codeforces: Square?

---
+++ Codeforces/Codeforces_Round_1062_(Div._4)/A_Square_.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Square?
+// Contest: 2167
+// URL: https://codeforces.com/contest/2167/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:11:31 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
