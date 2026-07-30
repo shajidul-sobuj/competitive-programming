d57ecdf592c5d96c1920d2944fba08b39b81a238
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:16:00 +0600

    [SyncForge] Auto-push Codeforces: Sum

---
+++ Codeforces/Codeforces_Round_827_(Div._4)/A_Sum.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Sum
+// Contest: 1742
+// URL: https://codeforces.com/contest/1742/problem/A
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:15:59 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
