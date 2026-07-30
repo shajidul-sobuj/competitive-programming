4b868e15675720476e864b93b0681a2b5bfcd3da
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:53:52 +0600

    [SyncForge] Auto-push Codeforces: ezraft and Array

---
+++ Codeforces/2246/B - ezraft_and_Array.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: ezraft and Array
+// Contest: 2246
+// URL: https://codeforces.com/contest/2246/problem/B
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:53:52 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
