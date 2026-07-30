53e0de4e5c62cf7de9a4b7f1c07fa6f153aef324
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:14:13 +0600

    [SyncForge] Auto-push Codeforces: Dora and Search

---
+++ Codeforces/Codeforces_Round_852_(Div._2)/C_Dora_and_Search.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Dora and Search
+// Contest: 1793
+// URL: https://codeforces.com/contest/1793/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 10:14:13 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
