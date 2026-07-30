94ed7bb62b16444b6a8ca08d325cbd15a71fa54a
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 22:19:25 +0600

    [SyncForge] Auto-push AtCoder: abc297 a

---
+++ AtCoder/Contest_ABC297/A_abc297_a.cpp
@@ -0,0 +1,31 @@
+/**
+ * Platform: AtCoder
+ * Problem: abc297 a
+ * Contest: ABC297
+ * URL: N/A
+ * Synced via SyncForge Extension
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,x;
+    cin >> n >> x;
+    int a[n];
+    for (int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+    for (int i=0;i<n-1;i++)
+    {
+        if (a[i+1]-a[i]<=x)
+        {
+            cout << a[i+1] << endl;
+            return 0;
+        }
+    }
+    cout << "-1\n";
+
+    return 0;
+}
