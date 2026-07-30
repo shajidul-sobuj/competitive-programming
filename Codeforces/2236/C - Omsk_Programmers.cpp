a6d7b277b7156afeedfafdbd4a9889c64dd482e2
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-30 20:55:07 +0600

    [SyncForge] Auto-push Codeforces: Omsk Programmers

---
+++ Codeforces/2236/C - Omsk_Programmers.cpp
@@ -0,0 +1,15 @@
+// Platform: Codeforces
+// Problem: Omsk Programmers
+// Contest: 2236
+// URL: https://codeforces.com/contest/2236/problem/C
+// Language: C++23 (GCC 14-64, msys2)
+// Synced via SyncForge Developer OS
+// Sync Time: 7/30/2026, 8:55:06 PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution code synced automatically
+    return 0;
+}
